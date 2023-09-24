8c4c02c5d6f70f0d820db3322996a1be1df87305
Atharv Kulkarni <apdk.2017@gmail.com>
2023-09-24 21:41:23 +0530

    Changed couple of things

--- src/portfolio.js
+++ src/portfolio.js
@@ -31,14 +31,7 @@ const greeting = {
 };
 
 const socialMediaLinks = [
-  /* Your Social Media Link */
-  // github: "https://github.com/ashutosh1919",
-  // linkedin: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
-  // gmail: "ashutoshhathidara98@gmail.com",
-  // gitlab: "https://gitlab.com/ashutoshhathidara98",
-  // facebook: "https://www.facebook.com/laymanbrother.19/",
-  // twitter: "https://twitter.com/ashutosh_1919",
-  // instagram: "https://www.instagram.com/layman_brother/"
+ 
 
   {
     name: "Github",
